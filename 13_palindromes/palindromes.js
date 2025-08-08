d8071e2b28cc01f2e876528116c187765d83721f
stse416 <43531621+stse416@users.noreply.github.com>
2025-08-07 20:01:57 -0400

    Check the palindromes.
    Oh boy, using string.reverse() sure would've been easier.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    const nonLetterDigit = /[^\w\d]/g
+    str = str.replaceAll(nonLetterDigit, '');
 
+    let front = 0;
+    let last = str.length-1;
+
+    while (front <= last) {
+        if (str.charAt(front) !== str.charAt(last)) return false;
+        front++;
+        last--;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
