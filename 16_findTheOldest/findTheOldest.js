203c7e19248a168c92bf4609a663e2b554f013fa
stse416 <43531621+stse416@users.noreply.github.com>
2025-08-09 18:45:02 -0400

    Practice reduce

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function findAge (yearOfBirth, yearOfDeath) {
+        yearOfDeath ??= new Date().getFullYear();
+        return yearOfDeath - yearOfBirth;
+    }
 
+    return people.reduce((oldest, curr) => {
+        let age = findAge(curr.yearOfBirth, curr.yearOfDeath);
+        return age > findAge(oldest.yearOfBirth, oldest.yearOfDeath) ? curr : oldest;
+    })
 };
 
 // Do not edit below this line
