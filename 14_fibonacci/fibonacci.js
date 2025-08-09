58db707e9d7a12bd95309d254a020425dcecbdcc
stse416 <43531621+stse416@users.noreply.github.com>
2025-08-09 17:47:43 -0400

    Fibonnaci`d

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member < 0) return "OOPS";
+    if (member == 0) return 0;
+    
+    let count = 1;
+    let pre = 0;
+    let sum = 1;
 
+    while (count < member) {
+        let temp = sum;
+
+        sum += pre;
+        pre = temp;
+        count++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
